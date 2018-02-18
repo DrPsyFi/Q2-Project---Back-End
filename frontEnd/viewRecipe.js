486fc424c29c5919585c67a37b3940a610987f73
Sunil Kakar <26909769+DrPsyFi@users.noreply.github.com>
2018-02-17 17:24:40 -0800

    view recipe update

--- frontEnd/viewRecipe.js
+++ frontEnd/viewRecipe.js
@@ -1,16 +1,17 @@
 const baseURL = 'http://localhost:3000'
-const recipeId = window.location.search.slice(4)
+
 function getThisRecipe(newRecipe) {
   let title = document.querySelector("#createHead")
   let recPic = document.querySelector('#recipePic')
   let ingredList = document.querySelector('#ingredients')
   let directions = document.querySelector('#instructions')
 
-  axios.get(`${baseURL}/recipes/${recipeId}`)
+  axios.get(`${baseURL}/recipes/`)
     .then(result => {
       console.log(result.data);
     title.innerText = result.data.name
-    
+    // const picImage = document.createElement('img')
+    // picImage.setAttribute('src', result.data.picture)
     recPic.setAttribute('src', result.data.picture)
     ingredList.innerText = "Ingredients are a coming soon feature"
     directions.innerText = result.data.instructions
